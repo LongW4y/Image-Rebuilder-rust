37697ccfceeba85d2d182043657e656d5ef46164
Cezary Stanislawski <cezary.marek.stanislawski@gmail.com>
2023-05-20 02:54:27 +0200

    optimizations
    rm image

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 
 use image::{DynamicImage, GenericImageView, GenericImage};
 use rand::random;
+use time::Duration;
 
 // Check if file exists
 fn file_exists(path: &str) -> bool {
@@ -47,39 +48,42 @@ fn create_black_image(path: &str, width: u32, height: u32) {
 
 // Generate N amount of random RGBA pixels
 fn generate_random_pixels(n: u32) -> Vec<(u8, u8, u8, u8)> {
-    let mut pixels = Vec::new();
-    for _ in 0..n {
-        let r = rand::random::<u8>();
-        let g = rand::random::<u8>();
-        let b = rand::random::<u8>();
-        let a = rand::random::<u8>();
-        pixels.push((r, g, b, a));
-    }
-    pixels
+    (0..n).map(|_| {
+        (
+            random::<u8>(), // R
+            random::<u8>(), // G
+            random::<u8>(), // B
+            random::<u8>(), // A
+        )
+    }).collect::<Vec<(u8, u8, u8, u8)>>()
 }
 
-// Calculate color distance between two pixels, as the square root of the sum of four differences raised to the second power
-fn calculate_distance(p1: (u8, u8, u8, u8), p2: (u8, u8, u8, u8)) -> f64 {
-    let r = (p1.0 as f64 - p2.0 as f64).powf(2.0);
-    let g = (p1.1 as f64 - p2.1 as f64).powf(2.0);
-    let b = (p1.2 as f64 - p2.2 as f64).powf(2.0);
-    let a = (p1.3 as f64 - p2.3 as f64).powf(2.0);
-    (r + g + b + a).sqrt()
+// Calculate color distance between two pixels, as the sum of four differences squared
+fn calculate_distance(p1: (u8, u8, u8, u8), p2: (u8, u8, u8, u8)) -> u32 {
+    let r = (p1.0 ^ p2.0).pow(2);
+    let g = (p1.1 ^ p2.1).pow(2);
+    let b = (p1.2 ^ p2.2).pow(2);
+    let a = (p1.3 ^ p2.3).pow(2);
+    (r + g + b + a) as u32
 }
 
 // Find the closest pixel to a givel pixel out of a vector of pixels
 fn find_closest_pixel(pixel: (u8, u8, u8, u8), pixels: &Vec<(u8, u8, u8, u8)>) -> [u8; 4] {
+    if pixels.len() == 0 {
+        return [0, 0, 0, 0]; // Return black pixel if there are no pixels
+    }
     let mut closest_pixel = pixels[0];
     let mut closest_distance = calculate_distance(pixel, pixels[0]);
+
     for p in pixels {
         let distance = calculate_distance(pixel, *p);
         if distance < closest_distance  {
             closest_pixel = *p;
             closest_distance = distance;
         }
-        if distance < 50.0  {
-            return [closest_pixel.0, closest_pixel.1, closest_pixel.2, closest_pixel.3]
-        }
+        // if distance < 50.0  {
+        //     return [closest_pixel.0, closest_pixel.1, closest_pixel.2, closest_pixel.3]
+        // }
     }
     [closest_pixel.0, closest_pixel.1, closest_pixel.2, closest_pixel.3]
 }
@@ -124,78 +128,63 @@ fn color_output_image(path: &str) {
     // Read input file
     let img = read_image(&path);
 
-    // Generate N amount of random RGBA pixels, where N is the number of pixels in the input image
-    let mut pixels = generate_random_pixels(img.width() * img.height());
-
-    // Create a black image of the same dimensions as the input image and return it
-    let output = "samples/output.png";
-    create_black_image(output, img.width(), img.height());
+    // Create a black image of the same dimensions as the input image and return it, name it ${source_file}_output.png
+    let output = format!("{}_output.{}", path.split(".").collect::<Vec<&str>>()[0], path.split(".").collect::<Vec<&str>>()[1]);
+    // Create a black image if $output does not exist
+    if !file_exists(&output) {
+        create_black_image(&output, img.width(), img.height());
+    } else {
+        // Notify the user that the output image already exists
+        println!("Output image already exists, it will be overwritten");
+    }
     // Read the black image, which will be the output image
-    let mut output_img = read_image(output);
+    let mut output_img = read_image(&output);
 
-    // Read the input image pixels
-    let input_pixels = read_image_pixels(&path);
+    // Generate N amount of random RGBA pixels, where N is the number of pixels in the input image
+    let mut random_pixels = generate_random_pixels(img.width() * img.height());
 
     // For each pixel in the input image, find the closest pixel in the random pixels vector
     // and color the pixel in the output image with the closest pixel at the same coordinates
-    let n: u32 = 1;
-    let mut i = 0;
 
-    // Check if n is equal to 1
-    if n == 1 {
-        // Sort the pixels by their red, green, blue, and alpha values
-        pixels.sort_by(|a, b| a.0.cmp(&b.0));
-        // Sort the input image pixels by their red, green, blue, and alpha values, which are in the second tuple
-        let mut input_pixels2 = input_pixels.clone();
-        input_pixels2.sort_by(|a, b| a.1.cmp(&b.1));
-        // For each pixel in the input image, find the closest pixel in the random pixels vector
-        // and color the pixel in the output image with the closest pixel at the same coordinates
-        for (i, pixel) in input_pixels2.iter().enumerate() {
-            // Get the closest pixel
-            let closest_pixel = find_closest_pixel(pixel.1, &pixels);
-            // Create a copy of closest_pixel as (u8, u8, u8, u8)
-            let closest_pixel2 = (closest_pixel[0], closest_pixel[1], closest_pixel[2], closest_pixel[3]);
+    let mut i = 0;
+    let n = 2; // N x N pixels
+    // Read the input image pixels
+    let input_pixels = read_image_pixels(&path);
+
+    // Define a step
+    // Loop through the input image's nth pixels horizontally and vertically
+    for x in 0..(img.width()/n) {
+        for y in 0..(img.height()/n) {
+            // Get the N pixels in a for loop
+            let mut p = Vec::new();
+            for k in 0..n {
+                for l in 0..n {
+                    let pixel = img.get_pixel(x * n + k, y * n + l);
+                    p.push((pixel.0[0], pixel.0[1], pixel.0[2], pixel.0[3]));
+                }
+            }
+            // Get the closest pixels
+            let avg = calculate_average_color(&p);
+            // Find the closest pixel to the average pixel
+            let closest_pixel = find_closest_pixel(avg, &random_pixels);
             // Delete the closest pixel from the random pixels vector
-            pixels.retain(|&x| x != closest_pixel2);
+            random_pixels.retain(|&x| x != (closest_pixel[0], closest_pixel[1], closest_pixel[2], closest_pixel[3]));
             // Color the output image pixel with the closest pixel
-            output_img.put_pixel(pixel.0 .0, pixel.0 .1, image::Rgba(closest_pixel));
-            // Print progress as pixels are colored
-            println!("Progress: {} / {}", i, img.width() * img.height());
-        }
-    } else {
-        for x in (0..(img.width()/n)-1).step_by(n as usize) {
-            for y in (0..(img.height()/n) - 1).step_by(n as usize) {
-                // Get the N pixels in a for loop
-                let mut p = Vec::new();
-                for k in 0..n {
-                    for l in 0..n {
-                        let pixel = img.get_pixel(x * n + k, y * n + l);
-                        p.push((pixel.0[0], pixel.0[1], pixel.0[2], pixel.0[3]));
-                    }
+            for k in 0..n {
+                for l in 0..n {
+                    output_img.put_pixel(x * n + k, y * n + l, image::Rgba(closest_pixel));
                 }
-                // Get the closest pixels
-                let avg = calculate_average_color(&p);
-                // Find the closest pixel to the average pixel
-                let closest_pixel = find_closest_pixel(avg, &pixels);
-                // Create a copy of closest_pixel as (u8, u8, u8, u8)
-                let closest_pixel2 = (closest_pixel[0], closest_pixel[1], closest_pixel[2], closest_pixel[3]);
-                // Delete the closest pixel from the random pixels vector
-                pixels.retain(|&x| x != closest_pixel2);
-                // Color the output image pixel with the closest pixel
-                for k in 0..n {
-                    for l in 0..n {
-                        output_img.put_pixel(x * n + k, y * n + l, image::Rgba(closest_pixel));
-                    }
-                }
-                // Print progress as N * N pixels are colored
-                i += n * n;
+            }
+            // Print progress as N^2
+            i += n.pow(2);
+            if i % 100 == 0 {
                 println!("Progress: {} / {}", i, img.width() * img.height());
             }
         }
     }
 
     // Save the output image
-    write_image(output, &output_img);
+    write_image(&output, &output_img);
 
     println!("Done");
     
@@ -206,6 +195,8 @@ fn color_output_image(path: &str) {
 fn main() {
     // Get input file path
     let input = read_input("Input file path:");
+    // Start the timer
+    let start = std::time::Instant::now();
     // Check if input file exists
     if !file_exists(&input) {
         println!("Input file does not exist");
@@ -217,6 +208,10 @@ fn main() {
         return;
     }
 
-    color_output_image(&input)
+    color_output_image(&input);
 
+    // Stop the timer
+    let duration = start.elapsed();
+    // Print the time it took to run the program
+    println!("Time elapsed: {:?}", duration);
 }
