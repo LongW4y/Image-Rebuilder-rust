2aa4142e2c35a6e70d48561a01352e2ddda49de6
Cezary Stanislawski <cezary.marek.stanislawski@gmail.com>
2023-05-20 14:41:49 +0200

    implemented caching of closest pixel to avg color
    this impacts negatively smaller images, but positively larger ones

--- src/main.rs
+++ src/main.rs
@@ -68,13 +68,11 @@ fn generate_random_pixels(n: u32) -> Vec<(u8, u8, u8, u8)> {
 
 // Calculate color distance between two pixels, as the sum of four differences squared
 fn calculate_distance(p1: (u8, u8, u8, u8), p2: (u8, u8, u8, u8)) -> u32 {
-    // Use bitwise operations instead of pow(2) for faster computation
-    let r = (p1.0 ^ p2.0) * (p1.0 ^ p2.0);
-    let g = (p1.1 ^ p2.1) * (p1.1 ^ p2.1);
-    let b = (p1.2 ^ p2.2) * (p1.2 ^ p2.2);
-    let a = (p1.3 ^ p2.3) * (p1.3 ^ p2.3);
-    // Use wrapping_add to avoid overflow checks
-    (r.wrapping_add(g).wrapping_add(b).wrapping_add(a)) as u32
+    let r = (p1.0 ^ p2.0).pow(2);
+    let g = (p1.1 ^ p2.1).pow(2);
+    let b = (p1.2 ^ p2.2).pow(2);
+    let a = (p1.3 ^ p2.3).pow(2);
+    (r + g + b + a) as u32
 }
 
 // Find the closest pixel to a givel pixel out of a vector of pixels
@@ -131,68 +129,71 @@ fn calculate_average_color(pixels: &Vec<(u8, u8, u8, u8)>) -> (u8, u8, u8, u8) {
 fn color_output_image(path: &str) {
     // Read input file
     let img = read_image(&path);
-
     // Create a black image of the same dimensions as the input image and return it, name it ${source_file}_output.png
     let output = format!("{}_output.{}", path.split(".").collect::<Vec<&str>>()[0], path.split(".").collect::<Vec<&str>>()[1]);
     // Create a black image if $output does not exist
     if !file_exists(&output) {
         create_black_image(&output, img.width(), img.height());
     } else {
-        // Notify the user that the output image already exists
+    // Notify the user that the output image already exists
         println!("Output image already exists, it will be overwritten");
     }
     // Read the black image, which will be the output image
     let mut output_img = read_image(&output);
-
     // Generate N amount of random RGBA pixels, where N is the number of pixels in the input image
     let mut random_pixels = generate_random_pixels(img.width() * img.height());
-
     // For each pixel in the input image, find the closest pixel in the random pixels vector
     // and color the pixel in the output image with the closest pixel at the same coordinates
-
     let mut i = 0;
-    let n = 2; // N x N pixels
+    let n = 2; // final image will be made of squares of n x n pixels
     // Read the input image pixels
     let input_pixels = read_image_pixels(&path);
-
-    // Define a step
-    // Loop through the input image's nth pixels horizontally and vertically
-    for x in 0..(img.width()/n) {
-        for y in 0..(img.height()/n) {
-            // Get the N pixels in a for loop
-            let mut p = Vec::new();
-            for k in 0..n {
-                for l in 0..n {
-                    let pixel = img.get_pixel(x * n + k, y * n + l);
-                    p.push((pixel.0[0], pixel.0[1], pixel.0[2], pixel.0[3]));
-                }
-            }
-            // Get the closest pixels
-            let avg = calculate_average_color(&p);
-            // Find the closest pixel to the average pixel
-            let closest_pixel = find_closest_pixel(avg, &random_pixels);
-            // Delete the closest pixel from the random pixels vector
-            random_pixels.retain(|&x| x != (closest_pixel[0], closest_pixel[1], closest_pixel[2], closest_pixel[3]));
-            // Color the output image pixel with the closest pixel
-            for k in 0..n {
-                for l in 0..n {
-                    output_img.put_pixel(x * n + k, y * n + l, image::Rgba(closest_pixel));
-                }
-            }
-            // Print progress as N^2
-            i += n.pow(2);
-            if i % 100 == 0 {
-                println!("Progress: {} / {}", i, img.width() * img.height());
-            }
-        }
+    // Use a hash map to store the closest pixels for each average color to avoid repeated calculations
+    use std::collections::HashMap;
+    let mut cache = HashMap::new();
+    for x in 0..img.width()/n {
+    for y in 0..img.height()/n {
+    // Get the N pixels in a for loop
+    let mut p = Vec::new();
+    for k in 0..n {
+    for l in 0..n {
+    let pixel = img.get_pixel(x * n + k, y * n + l);
+    p.push((pixel.0[0], pixel.0[1], pixel.0[2], pixel.0[3]));
     }
-
+    }
+    // Get the closest pixels
+    let avg = calculate_average_color(&p);
+    // Check if the cache contains the closest pixel for this average color
+    let closest_pixel = match cache.get(&avg) {
+    Some(pixel) => *pixel, // Use the cached value if it exists
+    None => { // Otherwise, find the closest pixel and store it in the cache
+    let pixel = find_closest_pixel(avg, &random_pixels);
+    cache.insert(avg, pixel);
+    pixel
+    }
+    };
+    // Delete the closest pixel from the random pixels vector
+    random_pixels.retain(|&x| x != (closest_pixel[0], closest_pixel[1], closest_pixel[2], closest_pixel[3]));
+    // Color the output image pixel with the closest pixel
+    for k in 0..n {
+    for l in 0..n {
+    output_img.put_pixel(x * n + k, y * n + l, image::Rgba(closest_pixel));
+    }
+    }
+    // Print progress as N^2
+    i += n * n;
+    if i % 100 == 0 {
+        println!("Progress: {} / {}", i, img.width() * img.height());
+    }
+    }
+    }
+    
     // Save the output image
     write_image(&output, &output_img);
-
+    
     println!("Done");
     
-}
+    }
 
 
 // Main
